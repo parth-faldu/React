2225141bae03466067fdca7bdc462f24f672d3c9
parth-faldu <parth123faldu@gmail.com>
2025-01-26 15:17:07 +0530

    Add console log for form data in contactData function

--- Excel_Flix/src/pages/Contact.jsx
+++ Excel_Flix/src/pages/Contact.jsx
@@ -4,6 +4,7 @@ export const contactData = async ({ request }) => {
     try {
         const res = await request.formData();
         const data = Object.fromEntries(res);
+        console.log(data);
     } catch (error) {
         console.log(error);
     }
