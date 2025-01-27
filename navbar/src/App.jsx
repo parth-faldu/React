526bb4111d8c40708b020f116bf8d7b6791a399b
parth-faldu <parth123faldu@gmail.com>
2025-01-27 17:54:26 +0530

    Add initial setup for Navbar project with Vite and React; include main components, styles, and configuration files

---
+++ navbar/src/App.jsx
@@ -0,0 +1,14 @@
+import { HeroSection } from "./components/HeroSection";
+import { NavBar } from "./components/NavBar";
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <HeroSection />
+    </>
+  );
+};
+
+export default App;
---
+++ navbar/src/components/NavBar.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
+
+export const NavBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleButtonToggle = () => {
+    setShowMenu(!showMenu);
+  };
+  return (
+    <>
+      <header>
+        <div className="container">
+          <div className="grid navbar-grid">
+            <div className="logo">
+              <h1>REACT APP</h1>
+            </div>
+
+            <nav className={showMenu ? "menu-mobile" : "menu-web"}>
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">About</a>
+                </li>
+                <li>
+                  <a href="#">Service</a>
+                </li>
+                <li>
+                  <a href="#">Contact</a>
+                </li>
+              </ul>
+            </nav>
+
+            <div className="ham-menu">
+              <button onClick={handleButtonToggle}>
+                <GiHamburgerMenu />
+              </button>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
